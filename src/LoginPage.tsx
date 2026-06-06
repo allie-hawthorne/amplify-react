5ae6c8eeb7ff91f59e461f975584c5bcc3e94511
Allie <me@alliehawthorne.dev>
2026-06-06 15:00:40 +0100

    SignInUpButton component

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -9,6 +9,17 @@ const Input = ({setState, ...inputProps}: InputProps) => {
   return <input className="bg-gray-700 p-2 rounded-lg" {...inputProps} placeholder={placeholder ?? type} onChange={e => setState(e.target.value)} />;
 }
 
+interface SignInUpButtonProps {
+  isRegister: boolean,
+  signIn: () => void,
+  signUp: () => void,
+}
+const SignInUpButton = ({isRegister, signIn, signUp}: SignInUpButtonProps) => {
+  return <button className="bg-pink-300 py-2 px-4 rounded-2xl" type="submit" onClick={isRegister ? signUp : signIn}>
+    Sign {isRegister ? "Up" : "In"}
+  </button>;
+}
+
 interface LoginPageProps {
   setPage: (page: string) => void;
 }
@@ -44,10 +55,7 @@ export const LoginPage = ({ setPage }: LoginPageProps) => {
       <Input type="password" value={password} setState={setPassword} />
       {isRegister && <Input type="password" placeholder="confirm password" value={confirmPassword} setState={setConfirmPassword} />}
       <div className="flex flex-row-reverse w-full justify-between">
-        {isRegister
-          ? <button className="bg-pink-300 py-2 px-4 rounded-2xl" type="submit" onClick={handleSignUp}>Sign Up</button>
-          : <button className="bg-pink-300 py-2 px-4 rounded-2xl" type="submit" onClick={handleSignIn}>Sign In</button>
-        }
+        <SignInUpButton isRegister={isRegister} signIn={handleSignIn} signUp={handleSignUp} />
         <button className="text-pink-300 opacity-70" onClick={() => setIsRegister(!isRegister)}>
           {isRegister ? 'Already have an account?' : "Don't have an account?"}
         </button>
