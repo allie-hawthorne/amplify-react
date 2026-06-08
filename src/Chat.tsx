474a06500114f846d7f2a230da1eb1d7a057b57a
Allie <me@alliehawthorne.dev>
2026-06-08 20:46:01 +0100

    submitting post request

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from 'react';
+import { post } from 'aws-amplify/api';
+import { Button, Input } from './shared';
+import { fetchAuthSession } from 'aws-amplify/auth';
+
+const API_NAME = 'aws-jess';
+const PATH = '/message';
+
 export const Chat = () => {
+  const [message, setMessage] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    fetchAuthSession().then(s => setToken(s.tokens?.accessToken?.toString() ?? ''));    
+  }, []);
+  
+  async function submitMessage() {
+    const response = await post({
+      apiName: API_NAME,
+      path: PATH,
+      options: {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        body: { message },
+      },
+    }).response;
+
+    const data = await response.body.json();
+
+    console.log(data)
+  }
+  
   return <>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, exercitationem.</p>
+    <form className='flex flex-col gap-2' onSubmit={e => e.preventDefault()}>
+      <Input placeholder='message' value={message} setState={setMessage} />
+      <Button onClick={submitMessage}>Submit</Button>
+    </form>
   </>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,15 @@ Amplify.configure({
       userPoolId: 'us-east-1_vgfda1CTg',
       userPoolClientId: '6c28aanu9bj3s56g7q3ee974u4',
     }
-  }
+  },
+  API: {
+    REST: {
+      "aws-jess": {
+        endpoint: 'https://3gjy989wfc.execute-api.us-east-1.amazonaws.com',
+        region: 'us-east-1',
+      },
+    },
+  },
 });
 
 createRoot(document.getElementById('root')!).render(
